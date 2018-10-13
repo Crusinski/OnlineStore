6f42a7608ffe41c44e4ca38bec932ede2ad3bf96
Crusinski <34046024+Crusinski@users.noreply.github.com>
2018-10-13 16:31:18 -0400

    added models and created Superuser

--- store/models.py
+++ store/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Item(models.Model):
+    item_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+    def __str__(self):
+        return self.item_text
+
+
+class Price(models.Model):
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    price_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+    def __str__(self):
+        return self.price_text
